9dedca5e01025f3bc9c17b393ee8ada160b9dc20
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-12-04 10:44:16 +0000

    modified nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- app :{
-  head: {
-    script : [
-      {
-        src: 'https://www.google.com/recaptcha/enterprise.js?render=6LcCKiApAAAAAPKIabyfPC_4UnFrs379OqyHF_wo',
-        defer :true
-      }
-    ]
+  app :{
+   head: {
+     script : [
+       {
+         src: 'https://www.google.com/recaptcha/enterprise.js?render=6LcCKiApAAAAAPKIabyfPC_4UnFrs379OqyHF_wo',
+         defer :true
+       }
+     ]
+   },
   },
- },
-  devtools: { enabled: true },
-  modules : [
-    "@nuxtjs/tailwindcss",
-    "nuxt-vuefire",
-    '@pinia/nuxt'
-  ],
-  vuefire : {
-    auth: {
-      enabled: true
-    },
-    config: {
-      apiKey: 'AIzaSyCwGfi68PjVt5t2xa3uuWyu5xhBN908Bdw',
-      authDomain: 'gethired-7b51b.firebaseapp.com',
-      projectId: 'gethired-7b51b',
-      appId: '1:471290472159:web:212d28aa0d6339071e2b6a',
-      // there could be other properties depending on the project
-    },
-  },
-  tailwindcss: {
-    config: {
-      plugins: [require('@tailwindcss/forms')]
-    }
-  }
-
-})
+   devtools: { enabled: true },
+   modules : [
+     "@nuxtjs/tailwindcss",
+     "nuxt-vuefire",
+     '@pinia/nuxt'
+   ],
+   vuefire : {
+     auth: {
+       enabled: true
+     },
+     config: {
+       apiKey: 'AIzaSyCwGfi68PjVt5t2xa3uuWyu5xhBN908Bdw',
+       authDomain: 'gethired-7b51b.firebaseapp.com',
+       projectId: 'gethired-7b51b',
+       appId: '1:471290472159:web:212d28aa0d6339071e2b6a',
+       // there could be other properties depending on the project
+     },
+   },
+   tailwindcss: {
+     config: {
+       plugins: [require('@tailwindcss/forms')]
+     }
+   }
+ 
+ })
+ 
